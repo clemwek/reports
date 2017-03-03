b62b0a259aff93cbde39de67539aae2035a54080
Clement Wekesa <clemwek@gmail.com>
2017-03-03 14:25:44 +0300

    Complete processing for new staff

--- public/members/staff/process_ajax.php
+++ public/members/staff/process_ajax.php
@@ -34,17 +34,22 @@ switch (getMode ()) {
         if (empty ($id_number)) {
             $error_array['national_id'] = 'Please fill the national id field.';
              $error = true;
+        } elseif (strlen($id_number)>11 ) {
+            $error_array['national_id'] = 'Please make sure you have entered the exact number of digits in the ID.';
+             $error = true;
         }
         if (empty ($phone_number)) {
             $error_array['staff_phone_number'] = 'Please fill the phone number field.';
              $error = true;
         }
+        
+        $site_name = $session->site_name;
 
         //proccess/ save to the db
         if ($error === false) {
-            $new_staff = Site_staff::make ($date, $firstName, $lastName, $id_number, $phone_number);
+            $new_staff = Site_staff::make ($id =(INT) 0, $date,$site_name, $firstName, $lastName, $id_number, $phone_number);
 
-            if ($new_staff && $new_staff->create()) {
+            if ($new_staff && $new_staff->save()) {
                 echo '<div class="alert alert-success" role="alert">Success <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
             } else {
                 echo 'There was an error while saving the database!!!';
